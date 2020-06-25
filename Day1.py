18420ace4299f09ef3afed95943bc3db4619c536
Malay Agarwal <mma6077@psu.edu>
2020-06-25 16:28:44 -0400

    Finished Day 1

--- Day1.py
+++ Day1.py
@@ -1,1 +1,30 @@
-"print ("hello")" 
+###########################################
+# Author: Malay Agarwal
+# Problem: 
+# Write a function that takes a list of strings
+# and prints each on a new line in a frame
+# *******
+# *Hello*
+# *world*
+# *in   *
+# *an   *
+# *hour *
+# *******
+###########################################
+
+# starting simple for Day1 to get used to git
+def stringInFrame():
+    inputString = input("input a word/sentence: ")
+    convertedList = inputString.split(" ")
+    longestWord = 0
+    for elem in convertedList:
+        if len(elem) > longestWord:
+            longestWord = len(elem)
+    return('*'*(longestWord+2))
+    for elem in convertedList:
+        if len(elem) < longestWord:
+            elem = elem + (' ')*(longestWord - len(elem))
+        return('*'+elem+'*')
+    return('*'*(longestWord+2))
+
+print(stringInFrame)
