bb485447162cde858ae0fc718a2b1c655223a7bd
Malay Agarwal <mma6077@psu.edu>
2020-06-27 15:57:39 -0400

    Day 2

---
+++ Day2.py
@@ -0,0 +1,16 @@
+# ###########################################
+# # Author: Malay Agarwal
+# # Problem: 
+# # Given a list of numbers and a number k, 
+# # return whether any two numbers from the 
+# # list add up to k.
+# ###########################################
+
+def main():
+    k = 10
+    k2 = k//2
+    numbers = [1,53,2,7,3]
+    goodnums = {k-x for x in numbers if x<=k2} & {x for x in numbers if x>k2}
+    pairs = [(k-x, x) for x in goodnums]
+    return bool(pairs)
+print (main())
