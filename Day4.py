70d525eef37007aecbdbccfc9d4f6e5cf294b57c
Malay Agarwal <mma6077@psu.edu>
2020-06-28 14:09:10 -0400

    Day 4 Finished

--- Day4.py
+++ Day4.py
@@ -1,1 +1,13 @@
-
+###########################################
+# Author: Malay Agarwal
+# Problem: 
+# Given an array, rotate the array to the 
+# right by k steps, where k is non-negative.
+# ###########################################
+r = [1,2,3,4,5]
+s = 2
+def main(arr,k):
+    temparr = arr[-k:]
+    temparr += arr[:k+1]
+    print(temparr)
+main(r,s)
