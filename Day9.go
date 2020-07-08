65c2881a2e6ad78a02a8642dbf4cb7d62764eb28
MalayCreates <mma6077@psu.edu>
2020-07-08 12:03:14 -0400

    Day 9 Finished

---
+++ Day9.go
@@ -0,0 +1,33 @@
+///////////////////////////////////////////
+// Author: Malay Agarwal
+// Problem:
+// We are given two strings, A and B.
+// A shift on A consists of taking string
+// A and moving the leftmost character to
+// the rightmost position.
+//////////////////////////////////////////
+
+package main
+
+import (
+	"fmt"
+)
+
+func rotateString(A string, B string) bool {
+	var temp string
+	isIt := false
+	for i, _ := range A {
+		temp = A[i:]
+		temp = temp + A[0:i]
+		if temp == B {
+			isIt = true
+		}
+	}
+	return isIt
+}
+
+func main() {
+	strA := "abcde"
+	strB := "cdeab"
+	fmt.Println(rotateString(strA, strB))
+}
