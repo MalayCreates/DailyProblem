4e8dd443cfbe6a7f79dada4491e6174c01006faf
MalayCreates <mma6077@psu.edu>
2020-07-08 11:40:55 -0400

    Day 8 Finished

---
+++ Day8.go
@@ -0,0 +1,29 @@
+///////////////////////////////////////////
+// Author: Malay Agarwal
+// Problem:
+// Given a non-negative integer num,
+// repeatedly add all its digits until the
+// result has only one digit.
+//////////////////////////////////////////
+package main
+
+import (
+	"fmt"
+)
+
+func addDigits(num int) int {
+	for num > 9 {
+		temp := 0
+		for num > 0 {
+			temp += num % 10
+			num /= 10
+		}
+		num = temp
+	}
+	return num
+}
+
+func main() {
+	inNum := 39
+	fmt.Println(addDigits(inNum))
+}
